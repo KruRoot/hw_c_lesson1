f44c38794f8edbea4fda1d4c5cf3673780bd5aeb
Andrei <andreyyuryev@yahoo.com>
2022-08-06 21:07:27 +0400

    add hw #02

---
+++ lesson1_hw_02/Program.cs
@@ -0,0 +1,15 @@
+﻿//Homework for lesson 1, #2
+int max = 0;
+Console.WriteLine("Enter first number");
+int first = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second number");
+int second = int.Parse(Console.ReadLine());
+if (first < second)
+    {
+    max = second;
+    }
+else
+    {
+    max = first;
+    }
+Console.WriteLine($"Max number = {max}");
