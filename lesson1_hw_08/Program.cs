60787a388bd2fc9c6846f944ce5b2963328530b6
Andrei <andreyyuryev@yahoo.com>
2022-08-06 23:40:06 +0400

    add hw #8

---
+++ lesson1_hw_08/Program.cs
@@ -0,0 +1,13 @@
+﻿//Homework for lesson 1, #8
+int count = 0;
+List<int> even_nums = new List<int>();
+Console.WriteLine("Enter the number");
+int num = int.Parse(Console.ReadLine());
+for (int index = 1; index <= num; index++)
+    if (index % 2 == 0)
+        {
+        Console.WriteLine(index);
+        }
+    else {
+
+    }
