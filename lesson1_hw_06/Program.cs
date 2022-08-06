84292689f7adb717e01d952ad02cd13f76a7b391
Andrei <andreyyuryev@yahoo.com>
2022-08-06 21:44:48 +0400

    add hw #06

---
+++ lesson1_hw_06/Program.cs
@@ -0,0 +1,11 @@
+﻿//Homework for lesson 1, #6
+Console.WriteLine("Enter the number");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0)
+    {
+    Console.WriteLine("This number is even");
+    }
+else
+    {
+    Console.WriteLine("This number is odd");
+    }
